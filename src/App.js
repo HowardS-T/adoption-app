a6090123790deef82a68a2e448115f74e3442220
Howard Symes Thompson <howard.symes.thompson@gmail.com>
2024-01-26 12:22:35 +0000

    created components

---
+++ src/App.js
@@ -0,0 +1,23 @@
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
+  ]);
+};
+
+const App = () => {
+  return React.createElement(
+    "div",
+    {},
+    [
+      React.createElement("h1", {}, "Adopt Me!"),
+      React.createElement(Pet, {name: "Fanfan", animal: "Dog", breed: "Corgi",}),
+      React.createElement(Pet, {name: "Felix", animal: "Cat", breed: "Siamese",}),
+      React.createElement(Pet, {name: "Garfield", animal: "Cat", breed: "British Shorthair",}),
+    ]);
+};
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
