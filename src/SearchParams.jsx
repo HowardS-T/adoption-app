440977ed66d42df9ced63ac752e8f49f88f4f4f7
Howard Symes Thompson <howard.symes.thompson@gmail.com>
2024-01-30 16:26:55 +0000

    added breed dropdown to search form

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -5,6 +5,8 @@ const ANIMALS = ["Bird", "Cat", "Dog", "Rabbit", "Reptile"];
 const SearchParams = () => {
   const [location, setLocation] = useState("");
   const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const breeds = [];
   return (
     <div className="search-params">
       <form>
@@ -20,7 +22,10 @@ const SearchParams = () => {
         <label htmlFor="animal">
           Animal
           <select
-            onChange={(e) => setAnimal(e.target.value)}
+            onChange={(e) => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
             id="animal"
             value={animal}
           >
@@ -28,6 +33,22 @@ const SearchParams = () => {
             {ANIMALS.map((animal) => (
               <option key={animal} value={animal}>
                 {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            onChange={(e) => setBreed(e.target.value)}
+            id="breed"
+            disabled={breeds.length === 0}
+            value={breed}
+          >
+            <option />
+            {breeds.map((breed) => (
+              <option key={breed} value={breed}>
+                {breed}
               </option>
             ))}
           </select>
